d617b49bfac423fc01452e222e752031b43c1f2f
Rowland Mosbergen <rowland.mosbergen@gmail.com>
2016-01-04 16:26:29 +1000

    Task #2371 - Adding summary horizontal bar graph - different example for small

--- examples/main_example.js
+++ examples/main_example.js
@@ -5,49 +5,8 @@ function round_to_two_decimal_places(num){
     return new_num;
 }
 
-//An array of colours which are used for the different probes
-var colours = ["DarkOrchid", "Orange", "DodgerBlue", "Blue","BlueViolet","Brown", "Deeppink", "BurlyWood","CadetBlue",
-"Chartreuse","Chocolate","Coral","CornflowerBlue","Crimson","Cyan", "Red", "DarkBlue",
-"DarkGoldenRod","DarkGray", "Tomato", "Violet","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen",
-"DarkOrange","DarkOrchid","DarkRed","DarkSalmon","DarkSlateBlue","DarkTurquoise",
-"DarkViolet","DeepPink","DeepSkyBlue","DodgerBlue","FireBrick","ForestGreen","Fuchsia",
-"Gold","GoldenRod","Green","GreenYellow","HotPink","IndianRed","Indigo"];
-
-
-// tip which is displayed when hovering over a collumn. Displays the sample type 
-//of the collumn
-var tip = d3.tip()
-    .attr('class', 'd3-tip')
-    .html(function(d) {
-        sample_type = d.sample_type;
-        temp =
-            "Sample Type: " +  sample_type + "<br/>"
-        return temp;
-    });
-
-// this tooltip function is passed into the graph via the tooltip
-var tooltip = d3.tip()
-    .attr('class', 'd3-tip')
-    .offset([0, +110])
-    .html(function(d) {
-        probe = d.Probe;
-        // 2 decimal places on the display only
-        Expression_Value = round_to_two_decimal_places(d[y_column]);
-        lwr = round_to_two_decimal_places(d.Expression_Value - d.Standard_Deviation);
-        upr = round_to_two_decimal_places(d.Expression_Value + d.Standard_Deviation);
-        temp = 
-            "Sample: " + d.sample_id +"<br/>"+
-            "Dataset: " + d.ds_id +"<br/>"+
-            "YuGene Value: " + Expression_Value +"<br/>"
-           // "MSC predicted "+msc_call+"/"+total+" iterations<br/>"
-        return temp; 
-    });
-
 //The url's to the data displayed
 data_url= '../data/json_format_clec4e_human_dev_s4m_very_small.txt';
-
-
-
 
 /* Extracting the data from the csv files for use in the graph
  * Also sets relevent options based on the data passed in (for example
@@ -86,15 +45,15 @@ d3.json(data_url,function (error,data){
     });
     data = sort_by_number;
 
-    title = "Overall Summary Horizontal Bar Graph for Clec4e ";
-    subtitle1 = "Subtitle"
-    subtitle2 = "Subtitle"
+
+
+
+    title = "Sample Breakdown by Tissue";
     target = rootDiv;
-    height = 900;
-    width = 600;
-    ds_id_array =  [5008,3000,5003];
-    ds_id_array =  [5008];
-    ds_id_array =  [5001,4000,6062];
+    height = 500;
+    width = 800;
+    margin = {top: 50, left:20, bottom: 10, right: 20};
+
 
     //The main options for the graph
     var options = {
@@ -102,55 +61,21 @@ d3.json(data_url,function (error,data){
         background_colour: "white",
         background_stroke_colour:  "black",
         background_stroke_width:  "6px",
-        circle_radius: {small: 4, large: 3.5},  // for the scatter points
-        hover_circle_radius: 10,
-        colour: colours,
-        ds_id_array: ds_id_array,
         data: data,
-        domain_colours : ["#FFFFFF","#7f3f98"],
-        error_bar_width:5,
-        error_dividor:100,//100 means error bars will not show when error < 1% value 
         gap_between_groups: 2,
         height: height,
-        //horizontal lines takes a name, colour and the yvalue. If no colour is given one is chosen at random
+        height_margin_for_title : 25,
         horizontal_space_for_labels: 100, // this is for giving space on the left side of the bar graphs
-        horizontal_lines: [["Detection Threshold", "green", 5], ["Median", , 8.93]],
-        horizontal_line_value_column: 'value',
-        //to have horizontal grid lines = width (to span accross the grid), otherwise = 0
-        horizontal_grid_lines: width,
         inner_margin: 10, // margin between the bars and the outer box
-        legend_class: "legend",
-        legend_range: [0,100],
-        line_stroke_width: "2px",
-        margin_legend: width - 190,
-        margin:{top: 180, left:200, bottom: 230, right: 300},
-        margin_small:{top: 40, left: 40, bottom: 40, right: 80},
-        //default number of colours is 39 (before it reitterates over it again)
-        number_of_colours: 39,
-        //2 is the chosen padding. On either side there will be padding = to the interval between the points
-        //1 gives 1/2 the interval on either side etc.
-        padding: 2,
-        show_horizontal_line_labels: true,
-        subtitle1: subtitle1,
-        subtitle2: subtitle2,
-        stroke_width:"3px",
+        margin: margin,
         target: target,
-        text_size: {small: "12px", large: "20px"},
-        title_text_size: {small: "12px", large: "30px"},
         title: title,
         title_class: "title",
-        tip: tip,//second tip to just display the sample type
-        tooltip: tooltip, // using d3-tips
-        //tooltip1: tooltip1, // using d3-tips unique_id: "chip_id",
         watermark:"http://www1.stemformatics.org/img/logo.gif",
         width:  width, // suggest 50 per sample
-        x_axis_text_angle:-45, 
-        x_axis_title: "Samples",
-        x_column: 'x_position',
-        x_middle_title: 500,
-        y_axis_title: "YuGene Value",
-        y_column: 'yugene_value'
+        x_axis_title: "Samples Breakdown by Tissue"
     }
+
 
     var instance = new app(options);
 
